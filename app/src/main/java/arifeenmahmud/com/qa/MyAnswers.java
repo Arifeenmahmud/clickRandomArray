4551586ba3509f01ae606a2391abd4f3641a04cb
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2021-08-27 13:58:07 +0600

    Initial commit click Random array show

---
+++ app/src/main/java/arifeenmahmud/com/qa/MyAnswers.java
@@ -0,0 +1,27 @@
+package arifeenmahmud.com.qa;
+
+
+import java.util.Random;
+
+
+
+public class MyAnswers {
+
+    public String[]mAnswers ={
+            "Apple","Oranges","Mango","Banana","Strawberry","Apricot", "Malta",
+
+    };
+
+    //Methods which will be called to get all Answers
+
+
+    public String getAnswer() {
+
+        String answer;
+        Random randomGenerator = new Random();//Construct a new Random number generator
+        int randomNumber = randomGenerator.nextInt(mAnswers.length);
+        answer = mAnswers[randomNumber];
+        return answer;
+
+    }
+}
